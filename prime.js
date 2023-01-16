3323e09bebedc8a09d4b5d466056dfbcff809383
Rahul Sharma <rahusharma.sharma@gmail.com>
2023-01-16 22:53:00 +0530

    Prime file

---
+++ prime.js
@@ -0,0 +1,13 @@
+let count=0;
+  
+for(let i=1; i<=num; i++){
+   if(num%i==0){
+     count ++
+   } 
+}  
+if(count == 2){
+  console.log("Yes it is Prime");
+}
+else{
+  console.log("No, it is not prime")
+}  
